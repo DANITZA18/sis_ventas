b3bb8904697f322eaf44341eec9b9e9d253deec7
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-03 00:17:45 -0400

    editando el menu de empleados

--- resources/views/includes/menuAdmin.blade.php
+++ resources/views/includes/menuAdmin.blade.php
@@ -1,20 +1,20 @@
-<li class="{{request()->is('ventas*')? 'active':'active'}}">
+<li class="{{request()->is('ventas*')? 'active':''}}">
     <a href="{{route('home')}}">
     <i class="fa fa-dolly-flatbed"></i> <span>Ventas</span>
     </a>
 </li>
-<li class="{{request()->is('empleados*')? 'active':''}}">
-    <a href="{{route('home')}}">
+<li class="{{request()->is('users*')? 'active':''}}">
+    <a href="{{route('users.index')}}">
     <i class="fa fa-users"></i> <span>Empleados</span>
     </a>
 </li>
 <li class="{{request()->is('productos*')? 'active':''}}">
-    <a href="{{route('home')}}">
+    <a href="{{route('productos.index')}}">
     <i class="fa fa-wine-bottle"></i> <span>Productos</span>
     </a>
 </li>
 <li class="{{request()->is('descuentos*')? 'active':''}}">
-    <a href="{{route('home')}}">
+    <a href="{{route('descuentos.index')}}">
     <i class="fa fa-percentage"></i> <span>Descuentos</span>
     </a>
 </li>
