c592d26d56ebc903dc7cac55275b9acf7fd9a961
yovana <yovanita282.gcg65>
2020-04-03 00:12:00 -0300

    actualizando form

--- resources/views/empleados/forms/form.blade.php
+++ resources/views/empleados/forms/form.blade.php
@@ -22,6 +22,11 @@
     <div class="col-md-4">
         <label>Carnet de identidad*:</label>
             {{Form::number('ci',null,['class'=>'form-control','required'])}}
+            @if ($errors->has('ci'))
+            <span class="invalid-feedback" style="color:red;" role="alert">
+                <strong>{{ $errors->first('ci') }}</strong>
+            </span>
+            @endif
     </div>
     <div class="col-md-2">
         <label>Expedido*:</label>
