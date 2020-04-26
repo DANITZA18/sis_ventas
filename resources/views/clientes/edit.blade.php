7179c3887f66ab81fa20e958317be15694972a54
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-26 18:10:23 -0400

    subiendo archivo edit (1 parte)  y tambien archivo show(vacio) en vista

---
+++ resources/views/clientes/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('pagina')
+Modificar cliente
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{asset('css/subirFoto.css')}}">
+@endsection
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+           <h3 class="titulo_form">CLIENTES</h3>
+        </div>
+        
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="titulo_panel">MODIFICAR CLIENTE</h2>
+                </div>
+                <div class="panel-body">
+                {!! Form::model($cliente,['route'=>['clientes.update',$cliente->id],'method'=>'PUT','files'=>"true"]) !!}
+                    @include('clientes.forms.form')
+                    <div class="row">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-success">Actualizar</button>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
