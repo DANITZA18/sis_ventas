0149fe0fb89c1620e22d323eb7f95182a7b4e6ec
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-26 16:28:06 -0400

    subiendo segunda parte de cliente en controladores http

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -18,3 +18,28 @@ class ClienteController extends Controller
         }
         abort(401, 'Acceso no autorizado');
     }
+    public function create(Request $request)
+    {
+        if($request->user()->tipo == 'ADMINISTRADOR' || $request->user()->tipo == 'EMPLEADO')
+        {
+            return view('clientes.create');
+        }
+        abort(401, 'Acceso no autorizado');
+    }
+
+    public function store(ClienteStoreRequest $request)
+    {
+        $cliente = Cliente::create(array_map('mb_strtoupper',$request->all()));
+        $cliente->estado = 1;
+        $cliente->save();
+        return redirect()->route('clientes.index')->with('bien','Cliente registrado con éxito');
+    }
+
+    public function edit(Cliente $cliente, Request $request)
+    {
+        if($request->user()->tipo == 'ADMINISTRADOR' || $request->user()->tipo == 'EMPLEADO')
+        {
+            return view('clientes.edit',compact('cliente'));
+        }
+        abort(401, 'Acceso no autorizado');
+    }
