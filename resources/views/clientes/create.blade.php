b39598df2f358b68780c50290e59a2c463f56ac7
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-26 18:56:07 -0400

    subiendo 1 parte de create de clientes en vista

---
+++ resources/views/clientes/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('pagina')
+Registrar cliente
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{asset('css/subirFoto.css')}}">
+@endsection
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+           <h3 class="titulo_form">CLIENTES</h3>
+        </div>
