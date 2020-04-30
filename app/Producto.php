37ffc53d2bc4d0e48358b406835136ea81e346a3
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-30 00:16:52 -0400

    subiendo modelo de producto (app)

--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $fillable = [
+        'nom','costo','disponible','ingresos','salidas',
+        'descripcion'
+    ];
+
+    public function detalles()
+    {
+        return $this->hasMany('sis_ventas\DetalleVenta','producto_id','id');
+    }
+
+    public function promociones()
+    {
+        return $this->hasMany('sis_ventas\Promocion','producto_id','id');
+    }
 }
