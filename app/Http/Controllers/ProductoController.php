0e1636c6b0b19d05cbdefcf226c55e3bbdc9ff65
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-29 23:13:26 -0400

    subiendo funciones de crear y editar de productos en controladores

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -10,5 +10,40 @@ use sis_ventas\Promocion;
 
 class ProductoController extends Controller
 {
-    //   
+      public function index(Request $request)
+    {
+        if($request->user()->tipo == 'ADMINISTRADOR' || $request->user()->tipo == 'EMPLEADO')
+        {
+            $productos = Producto::all();
+            return view('productos.index',compact('productos'));
+        }
+        abort(401, 'Acceso no autorizado');
+    }
+
+    public function create(Request $request)
+    {
+        if($request->user()->tipo == 'ADMINISTRADOR' || $request->user()->tipo == 'EMPLEADO')
+        {
+            return view('productos.create');
+        }
+        abort(401, 'Acceso no autorizado');
+    }
+
+    public function store(Request $request)
+    {
+        $producto = Producto::create(array_map('mb_strtoupper',$request->all()));
+        $producto->disponible = $request->ingresos;
+        $producto->salidas = 0;
+        $producto->save();
+        return redirect()->route('productos.index')->with('bien','Producto registrado con éxito');
+    }
+
+    public function edit(Producto $producto, Request $request)
+    {
+        if($request->user()->tipo == 'ADMINISTRADOR' || $request->user()->tipo == 'EMPLEADO')
+        {
+            return view('productos.edit',compact('producto'));
+        }
+        abort(401, 'Acceso no autorizado');
+    }
 }
