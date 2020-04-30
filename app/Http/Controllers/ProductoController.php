1d8d1a767197e72713f33536acfe85026eb3125c
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-04-29 23:00:11 -0400

    subiendo primera parte de productos en controladores

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,6 +3,10 @@
 namespace sis_ventas\Http\Controllers;
 
 use Illuminate\Http\Request;
+use sis_ventas\Descuento;
+use sis_ventas\DetalleVenta;
+use sis_ventas\Producto;
+use sis_ventas\Promocion;
 
 class ProductoController extends Controller
 {
