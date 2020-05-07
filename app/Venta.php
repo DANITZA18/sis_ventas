c9dee96795b29847300cb4d61a2f4640d7129e83
yovana <yovanita282.gcg65@gmail.com>
2020-05-07 19:59:12 -0400

    agregando modulo ventas

--- app/Venta.php
+++ app/Venta.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venta extends Model
 {
-    //
+    protected $fillable = [
+        'empleado_id','cliente','nit','fecha_venta','nro_factura','total','total_final','qr',
+        'codigo_control'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('sis_ventas\User','user_id','id');
+    }
+
+    public function detalles()
+    {
+        return $this->hasMany('sis_ventas\DetalleVenta','venta_id','id');
+    }
+
+    public function promociones()
+    {
+        return $this->hasMany('sis_ventas\VentaPromocion','venta_id','id');
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo('sis_ventas\Cliente','cliente_id','id');
+    }
 }
