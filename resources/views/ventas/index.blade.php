6f3bf8176f39b26f93e8a15c51943fa73efb92bd
yovana <yovanita282.gcg65@gmail.com>
2020-05-07 20:27:21 -0400

    agregando vistas para ventas

---
+++ resources/views/ventas/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.admin')
+
+@section('pagina')
+Ventas
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+<!-- Content Header (Page header) -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+           <h3 class="titulo_form">VENTAS</h3>
+        </div>
+        
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="{{route('ventas.create')}}" class="btn btn-sm btn-success pull-right">
+                        <span>Nueva venta</span> <i class="fa fa-plus"></i>
+                    </a>
+                    <h2 class="titulo_panel">LISTA DE VENTAS</h2>
+                </div>
+                <div class="panel-body">
+                    @if(session('bien'))
+                    <div class="alert alert-success">
+                        <button data-dismiss="alert" class="close">&times;</button>
+                        {{session('bien')}}
+                    </div>
+                    @endif
+                    @if(session('uso'))
+                    <div class="alert alert-info">
+                        <button data-dismiss="alert" class="close">&times;</button>
+                        {{session('uso')}}
+                    </div>
+                    @endif
+                    <table class="data-table table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nº</th>
+                                <th>Empleado</th>
+                                <th>Cliente</th>
+                                <th>Fecha</th>
+                                <th>Total Bs.</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $cont =1;
+                            @endphp
+                            @foreach($ventas as $venta)
+                            <tr>
+                                <td>{{$cont++}}</td>
+                                <td>
+                                    @if($venta->user->empleado)
+                                    {{$venta->empleado->nombre}} {{$venta->empleado->paterno}} {{$venta->empleado->materno}}
+                                    @else
+                                    {{$venta->user->name}}
+                                    @endif
+                                </td>
+                                <td>
+                                    {{$venta->cliente->nombre}}
+                                </td>
+                                <td>
+                                    {{$venta->fecha_venta}}
+                                </td>
+                                <td>
+                                    {{$venta->total}}
+                                </td>
+                                <td class="btns-opciones">
+                                    <a href="{{route('ventas.show',$venta->id)}}" class="evaluar ver"><i class="fa fa-eye" data-toggle="tooltip" data-placement="left" title="Ver información"></i></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@include('modal.eliminar')
+
+@endsection
+
+@section('scripts')
+<script>
+    //DATA-TABLE
+    $('table.data-table').removeAttr('width').DataTable({
+        responsive: true,
+        columns : [
+            {width:"5%"},
+            null,
+            null,
+            null,
+            null,
+            {width:"5%"}
+        ],
+        scrollX: true,
+        scrollY: "400px",
+        scrollCollapse: true,
+        language: lenguaje,
+        pageLength:"25"
+    });
+ 
+
+</script>
+@endsection
