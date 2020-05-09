441dddc583ba65496f958543117bc79584188ef3
yovana <yovanita282.gcg65@gmail.com>
2020-05-08 20:15:53 -0400

    modificando detalle de ventas

--- app/DetalleVenta.php
+++ app/DetalleVenta.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetalleVenta extends Model
 {
-    //
+    protected $fillable = [
+        'producto_id','cantidad','costo','descuento_id','total'
+    ];
+    
+
+    public function producto()
+    {
+        return $this->belongsTo('sis_ventas\Producto','producto_id','id');
+    }
+
+    public function descuento()
+    {
+        return $this->belongsTo('sis_ventas\Descuento','descuento_id','id');
+    }
+
+    public function venta()
+    {
+        return $this->belongsTo('sis_ventas\Venta','venta_id','id');
+    }
 }
