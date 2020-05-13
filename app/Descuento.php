8c62af7057123b9b841de593061152250105705f
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2020-05-13 00:20:51 -0400

    modificando archivo descuentos en modales

--- app/Descuento.php
+++ app/Descuento.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Descuento extends Model
 {
-    //
+    protected $fillable = [
+        'nom','descuento','descripcion'
+    ];
+
+    public function detalles()
+    {
+        return $this->hasMany('sis_ventas\DetalleVenta','descuento_id','id');
+    }
+
+    public function promociones()
+    {
+        return $this->hasMany('sis_ventas\Promocion','descuento_id','id');
+    }
 }
