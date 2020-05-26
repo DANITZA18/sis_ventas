6034cc7d1e163eaa54eadbf07c83dee9e70f2999
yovana <yovanita282.gcg65@gmail.com>
2020-05-26 13:46:30 -0400

    modificando promociones

--- app/Promocion.php
+++ app/Promocion.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Promocion extends Model
 {
     protected $table = "promociones";
+
+    protected $fillable = [
+        'nom','producto_id','inicio','fin','descuento_id',
+        'fecha_inicio','fecha_fin'
+    ];
+    
+    public function producto()
+    {
+        return $this->belongsTo('sis_ventas\Producto','producto_id','id');
+    }
+
+    public function ventas()
+    {
+        return $this->hasMany('sis_ventas\VentaPromocion','promocion_id','id');
+    }
+
+    public function descuento()
+    {
+        return $this->belongsTo('sis_ventas\Descuento','descuento_id','id');
+    }
 }
